34c79fb6b8c846b51113da1b2c2a68a79a37939a
Colin Mackey <cmackey@captechconsulting.com>
2019-08-19 17:17:50 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mackey-dev-portfolio';
+}
