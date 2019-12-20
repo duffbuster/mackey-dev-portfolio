51fb62817250426b497ee4c4ca51defd8c77c3a0
Colin Mackey <cmackey@captechconsulting.com>
2019-12-20 10:01:50 -0500

    Added certs

---
+++ src/app/layout/certifications/certifications.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mdev-certifications',
+  templateUrl: './certifications.component.html',
+  styleUrls: ['./certifications.component.scss']
+})
+export class CertificationsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Test } from '@app/models';
+
 @Component({
   selector: 'mdev-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-  ngOnInit() {}
+  public test: Test = {
+    id: '1'
+  };
+  ngOnInit() {
+    console.log(this.test);
+  }
 }
