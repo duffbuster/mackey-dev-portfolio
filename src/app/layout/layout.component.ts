67d1be0b646b5d8a4c6373cccbdc2f03e8031a59
Colin Mackey <colin@fishbrains.com>
2020-07-06 16:18:22 -0400

    Fixed a compilation error

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Test } from '@app/models';
-
 @Component({
   selector: 'mdev-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  public test: Test = {
-    id: '1'
-  };
-  ngOnInit() {
-    console.log(this.test);
-  }
+  ngOnInit() {}
 }
